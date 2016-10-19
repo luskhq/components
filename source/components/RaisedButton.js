e0eb011718698183fcb7fab66b521d1328a188b6
Artur Müller <me@arturmuller.com>
2016-10-19 15:26:37 +0200

    feat(utils): Add `pick` and `apply` to stylesheet utils

--- source/components/RaisedButton.js
+++ source/components/RaisedButton.js
@@ -6,9 +6,7 @@ import Pressable from "./Pressable"
 
 import c from "classnames"
 import s from "../stylesheet"
-
-// Utility function to make working with map-like objects a bit easier
-const pick = (map, key) => map[key] || map.regular
+import {pick, apply} from "../stylesheet/utils"
 
 class RaisedButton extends React.Component {
 
@@ -25,50 +23,6 @@ class RaisedButton extends React.Component {
   render() {
     const {iconPosition, label, theme, disabled, pending, labelPending, size, icon, ...passingProps} = this.props
 
-    const sizes = {
-      large: s.fontSize("15"),
-      fullwidth: [
-        s.width("100perc"),
-        s.fontSize("16"),
-      ],
-      regular: s.fontSize("13"),
-    }
-
-    const themes = {
-      action: "oyster-blue",
-      create: "oyster-green",
-      danger: "oyster-red",
-      light: "white",
-      regular: "custom-gull-gray",
-    }
-
-    const applyTheme = (color) => {
-      if (disabled) {
-        return [
-          s.backgroundColor("custom-mischka-2"),
-          s.backgroundColor("custom-mischka-2", ":hover"),
-          s.boxShadow("none"),
-          s.boxShadow("none", ":hover"),
-        ]
-      }
-
-      if (pending) {
-        return [
-          s.backgroundColor(`${color}-dark`),
-          s.boxShadow("subtle-3"),
-        ]
-      }
-
-      return [
-        s.backgroundColor(color),
-        s.backgroundColor(`${color}-light`, ":hover"),
-        s.backgroundColor(`${color}-dark`, ":active"),
-        s.boxShadow("subtle-3"),
-        s.boxShadow("subtle-4", ":hover"),
-        s.boxShadow("subtle-3", ":active"),
-      ]
-    }
-
     return (
       <Pressable
         {...passingProps}
@@ -80,8 +34,48 @@ class RaisedButton extends React.Component {
           s.textTransform("uppercase"),
           s.borderRadius("3"),
           s.transition("background-shadow"),
-          pick(sizes, size),
-          applyTheme(pick(themes, theme)),
+          pick(size, {
+            regular: s.fontSize("13"),
+            large: s.fontSize("15"),
+            fullwidth: [
+              s.width("100perc"),
+              s.fontSize("16"),
+            ],
+          }),
+          apply(theme, (theme) => {
+            const color = pick(theme, {
+              regular: "custom-gull-gray",
+              action: "oyster-blue",
+              create: "oyster-green",
+              danger: "oyster-red",
+              light: "white",
+            })
+
+            if (disabled) {
+              return [
+                s.backgroundColor("custom-mischka-2"),
+                s.backgroundColor("custom-mischka-2", ":hover"),
+                s.boxShadow("none"),
+                s.boxShadow("none", ":hover"),
+              ]
+            }
+
+            if (pending) {
+              return [
+                s.backgroundColor(`${color}-dark`),
+                s.boxShadow("subtle-3"),
+              ]
+            }
+
+            return [
+              s.backgroundColor(color),
+              s.backgroundColor(`${color}-light`, ":hover"),
+              s.backgroundColor(`${color}-dark`, ":active"),
+              s.boxShadow("subtle-3"),
+              s.boxShadow("subtle-4", ":hover"),
+              s.boxShadow("subtle-3", ":active"),
+            ]
+          }),
       )}>
         <RaisedButtonInner
           iconPosition={iconPosition}
@@ -103,33 +97,31 @@ class RaisedButtonInner extends React.Component {
   render() {
     const {iconPosition, label, theme, pending, labelPending, size, icon} = this.props
 
-    const sizes = {
-      large: [
-        s.height("51"),
-        s.minWidth("135"),
-        s.paddingAll(["0", "24"]),
-      ],
-      condensed: [
-        s.height("36"),
-        s.paddingAll(["0", "12"]),
-      ],
-      fullwidth: [
-        s.height("54"),
-      ],
-      regular: [
-        s.height("45"),
-        s.minWidth("105"),
-        s.paddingAll(["0", "15"]),
-      ],
-    }
-
     return (
       <div className={c(
           s.display("flex"),
           s.alignItems("center"),
           s.justifyContent("center"),
           s.flexDirection(iconPosition === "left" && "row-reverse"),
-          pick(sizes, size),
+          pick(size, {
+            large: [
+              s.height("51"),
+              s.minWidth("135"),
+              s.paddingAll(["0", "24"]),
+            ],
+            condensed: [
+              s.height("36"),
+              s.paddingAll(["0", "12"]),
+            ],
+            fullwidth: [
+              s.height("54"),
+            ],
+            regular: [
+              s.height("45"),
+              s.minWidth("105"),
+              s.paddingAll(["0", "15"]),
+            ],
+          }),
         )}>
         <div className={c(
           s.color(theme === "light" ? "oyster-blue-gray" : "white"),
---
+++ source/stylesheet/utils.js
@@ -0,0 +1,50 @@
+/**
+ * Functional alternative to a switch/case declaration.
+ *
+ * Pass it a string and a map (object), and it will return the value found at
+ * the key which matches the supplied string.
+ *
+ * Falls back to object's `regular` key if present.
+ *
+ * @param {String} prop
+ * @param {Object} map
+ * @returns {*}
+ *
+ * @example
+ *
+ *    pick("danger", {
+ *      regular: "gray",
+ *      action: "green",
+ *      danger: "red",
+ *    })
+ *    // Returns "red"
+ *
+ *    pick("foo", {
+ *      regular: "gray",
+ *      action: "green",
+ *      danger: "red",
+ *    })
+ *    // Returns "gray"
+ *
+ */
+
+export const pick = (prop, map) => map[prop] || map.regular
+
+/**
+ * An alternative to IIFE (Immediately Invoked Function Expressions)
+ *
+ * Pass it a any value and a function and it will immediately call that
+ * function with the supplied argument
+ *
+ * @param {*}
+ * @param {Function}
+ * @returns {*}
+ *
+ * @example
+ *
+ *    apply({foo: 1, bar: 2}, (o) => o.foo + o.bar)
+ *    // Returns 3
+ *
+ */
+
+export const apply = (prop, func) => func(prop)
