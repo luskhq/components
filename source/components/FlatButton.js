9469e9fac32e86d2968ea789149cff17a06024b4
Patrik Illy <illy@Patriks-MacBook-Air.local>
2016-10-21 12:48:14 +0200

    refactor(FlatButton): use utils for styles

--- source/components/FlatButton.js
+++ source/components/FlatButton.js
@@ -6,9 +6,7 @@ import {Pressable} from "./index"
 
 import c from "classnames"
 import s from "../stylesheet"
-
-// Utility function to make working with map-like objects a bit easier
-const pick = (map, key) => map[key] || map.regular
+import {pick, apply} from "../stylesheet/utils"
 
 class FlatButton extends React.Component {
 
@@ -25,37 +23,6 @@ class FlatButton extends React.Component {
   render() {
     const {label, theme, active, disabled, pending, labelPending, size, icon, ...passingProps} = this.props
 
-    const sizes = {
-      large: s.fontSize("15"),
-      fullwidth: [
-        s.width("100perc"),
-        s.fontSize("16"),
-      ],
-      regular: s.fontSize("13"),
-    }
-
-    const applyTheme = () => {
-      if (disabled) {
-        return [
-          s.backgroundColor("custom-mischka-2"),
-          s.backgroundColor("custom-mischka-2", ":hover"),
-        ]
-      }
-
-      if (pending || active) {
-        return [
-          s.backgroundColor("black-alpha-15"),
-          s.backgroundColor("black-alpha-5", ":hover"),
-        ]
-      }
-
-      return [
-        s.backgroundColor("transparent"),
-        s.backgroundColor("black-alpha-5", ":hover"),
-        s.backgroundColor("black-alpha-15", ":active"),
-      ]
-    }
-
     return (
       <Pressable
         {...passingProps}
@@ -68,8 +35,35 @@ class FlatButton extends React.Component {
             s.boxShadow("none"),
             s.borderRadius("3"),
             s.transition("background"),
-            pick(sizes, size),
-            applyTheme(),
+            pick(size, {
+              regular: s.fontSize("13"),
+              large: s.fontSize("15"),
+              fullwidth: [
+                s.width("100perc"),
+                s.fontSize("16"),
+              ],
+            }),
+            apply(() => {
+              if (disabled) {
+                return [
+                  s.backgroundColor("custom-mischka-2"),
+                  s.backgroundColor("custom-mischka-2", ":hover"),
+                ]
+              }
+
+              if (pending || active) {
+                return [
+                  s.backgroundColor("black-alpha-15"),
+                  s.backgroundColor("black-alpha-5", ":hover"),
+                ]
+              }
+
+              return [
+                s.backgroundColor("transparent"),
+                s.backgroundColor("black-alpha-5", ":hover"),
+                s.backgroundColor("black-alpha-15", ":active"),
+              ]
+            }),
         )}>
         <FlatButtonInner
           label={label}
@@ -90,49 +84,53 @@ class FlatButtonInner extends React.Component {
   render() {
     const {label, theme, pending, labelPending, size, icon} = this.props
 
-    const sizes = {
-      large: [
-        s.height("51"),
-        s.minWidth("135"),
-        s.paddingAll(["0", "24"]),
-      ],
-      condensed: [
-        s.height("36"),
-        s.paddingAll(["0", "12"]),
-      ],
-      fullwidth: [
-        s.height("54"),
-      ],
-      regular: [
-        s.height("45"),
-        s.minWidth("105"),
-        s.paddingAll(["0", "15"]),
-      ],
-    }
-
     return (
       <div className={c(
           s.display("flex"),
           s.alignItems("center"),
           s.justifyContent("center"),
-          pick(sizes, size),
+          pick(size, {
+            regular: [
+              s.height("45"),
+              s.minWidth("105"),
+              s.paddingAll(["0", "15"]),
+            ],
+            large: [
+              s.height("51"),
+              s.minWidth("135"),
+              s.paddingAll(["0", "24"]),
+            ],
+            fullwidth: s.height("54"),
+            condensed: [
+              s.height("36"),
+              s.paddingAll(["0", "12"]),
+            ],
+          }),
         )}>
         <div className={c(
-          s.color("gray"),
-          s.color(theme === "light" && "white"),
-          s.color(theme === "action" && "oyster-blue"),
-          s.color(theme === "create" && "oyster-red"),
           s.fontWeight("600"),
+          apply(theme, (theme) => {
+            const color = pick(theme, {
+              regular: "gray",
+              light: "white",
+              action: "oyster-blue",
+              create: "oyster-red",
+            })
+
+            return [
+              s.color(`${color}`),
+            ]
+          }),
         )}>
           {(pending) ? labelPending : label}
         </div>
-        {(icon) ?
+        {(icon) &&
           <div className={c(
             s.marginAll(["auto", "auto", "auto", "9"]),
           )}>
             <SmallIcon name={icon} theme={theme} size="small" />
           </div>
-          : null}
+        }
       </div>
     )
   }
